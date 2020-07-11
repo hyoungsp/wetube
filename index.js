adf6aa915a82820bdf9964ccc921ca0987f66e8a
hyoungsun <hyoungsun@uchicago.edu>
2020-07-10 23:53:24 -0400

    add babel and nodemon for server dev dependencies

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hi");
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) =>
+  res.send("Hi Stanley from Home !!!!!!!!!!!!!! ");
+
+const handleProfile = (req, res) => res.send("you are on my profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening);
