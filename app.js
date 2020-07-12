83a368c3cfa1dc4070e70954ceb135061f2edcb7
hyoungsun <hyoungsun@uchicago.edu>
2020-07-11 22:17:38 -0400

    add routers and controllers; local middleware; font awesome integrated

--- app.js
+++ app.js
@@ -7,17 +7,22 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
-app.set("view engine", "pug");
+// app.use(function (req, res, next) {
+//   req.headers["if-none-match"] = "no-match-for-this";
+//   next();
+// });
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
-app.use("/user", userRouter);
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
